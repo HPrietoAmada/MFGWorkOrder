5f4e7cd7c525c96867a74abbd09cbdff32a4a9c3
Heriberto Prieto <HPrieto@amada.com>
2019-11-22 12:00:26 -0800

    Initial Commit

---
+++ MFGWorkOrder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MFGWorkOrder
+//
+//  Created by IT Support on 11/22/19.
+//  Copyright © 2019 IT Support. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
